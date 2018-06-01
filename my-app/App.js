52f83115fc57dd71e63bfad29a6c3f62f451c195
Marcus Kopp <marcus.kopp86@gmail.com>
2018-06-01 12:01:42 +0200

    Alphabet registry prototype
    - recognize touch event location
    - displays color of field in tag

--- my-app/App.js
+++ my-app/App.js
@@ -1,61 +1,48 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  StatusBar,
   AppRegistry,
+  Button,
+  Dimensions,
+  PanResponder,
+  StatusBar,
   StyleSheet,
   Text,
-  View,
-  PanResponder,
   TouchableOpacity,
-  Dimensions,
-} from "react-native";
+  View
+} from 'react-native';
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
-const getDirectionAndColor = ({ moveX, moveY, dx, dy }) => {
-  const draggedDown = dy > 30;
-  const draggedUp = dy < -30;
-  const draggedLeft = dx < -30;
-  const draggedRight = dx > 30;
-  const isRed = moveY < 90 && moveY > 40 && moveX > 0 && moveX < width;
-  const isBlue = moveY > height - 50 && moveX > 0 && moveX < width;
-  let dragDirection = "";
+const getDirectionAndColor = ({moveX, moveY, dx, dy }) => {
+  const isA = moveY > 50 && moveY <= 150 && moveX > 0 && moveX < 50;
+  const isB = moveY > 150 && moveY <= 250 && moveX > 0 && moveX < 50;
+  const isC = moveY > 250 && moveY <= 350 && moveX > 0 && moveX < 50;
 
-  if (draggedDown || draggedUp) {
-    if (draggedDown) dragDirection += "dragged down ";
-    if (draggedUp) dragDirection += "dragged up ";
-  }
-
-  if (draggedLeft || draggedRight) {
-    if (draggedLeft) dragDirection += "dragged left ";
-    if (draggedRight) dragDirection += "dragged right ";
-  }
-
-  if (isRed) return `red ${dragDirection}`;
-  if (isBlue) return `blue ${dragDirection}`;
-  if (dragDirection) return dragDirection;
+  if (isA) return 'Magenta'; else if (isB) return 'Orange'; else if (isC) return 'Blue';
 };
 
 export default class App extends Component {
   state = {
-    zone: "Still Touchable",
+    zone: 'Still Touchable'
   };
+
+
   componentWillMount() {
     this._panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: (evt, gestureState) => !!getDirectionAndColor(gestureState),
       onPanResponderMove: (evt, gestureState) => {
         const drag = getDirectionAndColor(gestureState);
         this.setState({
-          zone: drag,
+          zone: drag
         });
       },
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderTerminationRequest: (evt, gestureState) => true
     });
   }
 
   onPress = () => {
     this.setState({
-      zone: "I got touched with a parent pan responder",
+      zone: 'I got touched with a parent pan responder'
     });
   };
 
@@ -63,13 +50,14 @@ export default class App extends Component {
     return (
       <View style={styles.container} {...this._panResponder.panHandlers}>
         <StatusBar hidden />
-        <View style={styles.zone1} />
         <View style={styles.center}>
           <TouchableOpacity onPress={this.onPress}>
             <Text>{this.state.zone}</Text>
           </TouchableOpacity>
         </View>
+        <View style={styles.zone1} />
         <View style={styles.zone2} />
+        <View style={styles.zone3} />
       </View>
     );
   }
@@ -77,27 +65,38 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   center: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   zone1: {
-    top: 40,
+    top: 50,
     left: 0,
     right: 0,
-    height: 50,
-    position: "absolute",
-    backgroundColor: "red",
+    width: 50,
+    height: 100,
+    position: 'absolute',
+    backgroundColor: 'magenta'
   },
   zone2: {
+    top: 150,
     left: 0,
     right: 0,
-    bottom: 0,
-    height: 50,
-    position: "absolute",
-    backgroundColor: "blue",
+    width: 50,
+    height: 100,
+    position: 'absolute',
+    backgroundColor: 'orange'
   },
+  zone3: {
+    top: 250,
+    left: 0,
+    right: 0,
+    width: 50,
+    height: 100,
+    position: 'absolute',
+    backgroundColor: 'blue'
+  }
 });

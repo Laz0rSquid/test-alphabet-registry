9a9e2836e85635b3b956ef60ce96329da4a2d658
Marcus Kopp <marcus.kopp86@gmail.com>
2018-06-01 14:46:53 +0200

    Scrollable list, displays name of color, still off by one element

--- my-app/App.js
+++ my-app/App.js
@@ -12,20 +12,79 @@ import {
 } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
+const statBarHeight = 50;
+const registryElemWidth = 30;
+const registryElemHeight = (height - statBarHeight) / 27;
 
-const getDirectionAndColor = ({moveX, moveY, dx, dy }) => {
-  const isA = moveY > 50 && moveY <= 150 && moveX > 0 && moveX < 50;
-  const isB = moveY > 150 && moveY <= 250 && moveX > 0 && moveX < 50;
-  const isC = moveY > 250 && moveY <= 350 && moveX > 0 && moveX < 50;
+const getDirectionAndColor = ({ moveX, moveY, dx, dy }) => {
+  const isA =
+    moveY > statBarHeight &&
+    moveY <= statBarHeight + registryElemHeight &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isB =
+    moveY > statBarHeight + registryElemHeight &&
+    moveY <= statBarHeight + (2 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isC =
+    moveY > statBarHeight + (2 * registryElemHeight) &&
+    moveY <= statBarHeight + (3 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isD =
+    moveY > statBarHeight + (3 * registryElemHeight) &&
+    moveY <= statBarHeight + (4 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isE =
+    moveY > statBarHeight + (4 * registryElemHeight) &&
+    moveY <= statBarHeight + (5 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isF =
+    moveY > statBarHeight + (5 * registryElemHeight) &&
+    moveY <= statBarHeight + (6 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isG =
+    moveY > statBarHeight + (6 * registryElemHeight) &&
+    moveY <= statBarHeight + (7 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isH =
+    moveY > statBarHeight + (7 * registryElemHeight) &&
+    moveY <= statBarHeight + (8 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isI =
+    moveY > statBarHeight + (8 * registryElemHeight) &&
+    moveY <= statBarHeight + (9 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
+  const isJ =
+    moveY > statBarHeight + (9 * registryElemHeight) &&
+    moveY <= statBarHeight + (10 * registryElemHeight) &&
+    moveX > 0 &&
+    moveX < registryElemWidth;
 
-  if (isA) return 'Magenta'; else if (isB) return 'Orange'; else if (isC) return 'Blue';
+  if (isA) return 'Magenta';
+  else if (isB) return 'Orange';
+  else if (isC) return 'Blue';
+  else if (isD) return 'Green';
+  else if (isE) return 'Red';
+  else if (isF) return 'Yellow';
+  else if (isG) return 'Teal';
+  else if (isH) return 'Purple';
+  else if (isI) return 'Pink';
+  else if (isJ) return 'Plum';
+  else return 'Color'
 };
 
 export default class App extends Component {
   state = {
-    zone: 'Still Touchable'
+    zone: 'Color'
   };
-
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
@@ -50,14 +109,21 @@ export default class App extends Component {
     return (
       <View style={styles.container} {...this._panResponder.panHandlers}>
         <StatusBar hidden />
-        <View style={styles.center}>
+        <View style={(styles.center, styles.skyblue)}>
           <TouchableOpacity onPress={this.onPress}>
             <Text>{this.state.zone}</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.zone1} />
-        <View style={styles.zone2} />
-        <View style={styles.zone3} />
+        <View style={[styles.zone, styles.magenta]} />
+        <View style={[styles.zone, styles.orange]} />
+        <View style={[styles.zone, styles.blue]} />
+        <View style={[styles.zone, styles.green]} />
+        <View style={[styles.zone, styles.red]} />
+        <View style={[styles.zone, styles.yellow]} />
+        <View style={[styles.zone, styles.teal]} />
+        <View style={[styles.zone, styles.purple]} />
+        <View style={[styles.zone, styles.pink]} />
+        <View style={[styles.zone, styles.plum]} />
       </View>
     );
   }
@@ -68,35 +134,44 @@ const styles = StyleSheet.create({
     flex: 1
   },
   center: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: statBarHeight,
+    alignItems: 'center'
   },
-  zone1: {
-    top: 50,
-    left: 0,
-    right: 0,
-    width: 50,
-    height: 100,
-    position: 'absolute',
-    backgroundColor: 'magenta'
+  zone: {
+    width: registryElemWidth,
+    height: registryElemHeight
   },
-  zone2: {
-    top: 150,
-    left: 0,
-    right: 0,
-    width: 50,
-    height: 100,
-    position: 'absolute',
-    backgroundColor: 'orange'
+  skyblue: {
+    backgroundColor: '#0dd'
   },
-  zone3: {
-    top: 250,
-    left: 0,
-    right: 0,
-    width: 50,
-    height: 100,
-    position: 'absolute',
-    backgroundColor: 'blue'
+  magenta: {
+    backgroundColor: '#f0f'
+  },
+  orange: {
+    backgroundColor: '#f70'
+  },
+  blue: {
+    backgroundColor: '#00F'
+  },
+  green: {
+    backgroundColor: '#0f0'
+  },
+  red: {
+    backgroundColor: '#f00'
+  },
+  yellow: {
+    backgroundColor: '#ff0'
+  },
+  teal: {
+    backgroundColor: '#0af'
+  },
+  purple: {
+    backgroundColor: '#70f'
+  },
+  pink: {
+    backgroundColor: '#f07'
+  },
+  plum: {
+    backgroundColor: '#707'
   }
 });

c39305648c83494e605e367f715d15f0c0920ab5
Marcus Kopp <marcus.kopp86@gmail.com>
2018-06-05 12:35:00 +0200

    Fixes to Stat Bar

--- my-app/App.js
+++ my-app/App.js
@@ -31,84 +31,13 @@ const getSectionHelper = (moveX, moveY, counter) => {
   } else return getSectionHelper(moveX, moveY, counter+1);
 };
 
-const getColor = ({ moveX, moveY, dx, dy }) => {
-  const isA =
-    moveY > statBarHeight &&
-    moveY <= statBarHeight + registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isB =
-    moveY > statBarHeight + registryElemHeight &&
-    moveY <= statBarHeight + 2 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isC =
-    moveY > statBarHeight + 2 * registryElemHeight &&
-    moveY <= statBarHeight + 3 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isD =
-    moveY > statBarHeight + 3 * registryElemHeight &&
-    moveY <= statBarHeight + 4 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isE =
-    moveY > statBarHeight + 4 * registryElemHeight &&
-    moveY <= statBarHeight + 5 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isF =
-    moveY > statBarHeight + 5 * registryElemHeight &&
-    moveY <= statBarHeight + 6 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isG =
-    moveY > statBarHeight + 6 * registryElemHeight &&
-    moveY <= statBarHeight + 7 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isH =
-    moveY > statBarHeight + 7 * registryElemHeight &&
-    moveY <= statBarHeight + 8 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isI =
-    moveY > statBarHeight + 8 * registryElemHeight &&
-    moveY <= statBarHeight + 9 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-  const isJ =
-    moveY > statBarHeight + 9 * registryElemHeight &&
-    moveY <= statBarHeight + 10 * registryElemHeight &&
-    moveX > 0 &&
-    moveX < registryElemWidth;
-
-  if (isA) return 'Magenta';
-  else if (isB) return 'Orange';
-  else if (isC) return 'Blue';
-  else if (isD) return 'Green';
-  else if (isE) return 'Red';
-  else if (isF) return 'Yellow';
-  else if (isG) return 'Teal';
-  else if (isH) return 'Purple';
-  else if (isI) return 'Pink';
-  else if (isJ) return 'Plum';
-  else return 'Color';
-};
 
 export default class App extends Component {
-  state = {
-    zone: 'Color'
-  };
 
   componentWillMount() {
     this._panResponder = PanResponder.create({
-      onMoveShouldSetPanResponder: (evt, gestureState) => !!getColor(gestureState),
+      onMoveShouldSetPanResponder: (evt, gestureState) => !!getSection(gestureState,0),
       onPanResponderMove: (evt, gestureState) => {
-        const color = getColor(gestureState);
-        this.setState({
-          zone: color
-        })
         const section = getSection(gestureState,0);
         if (section > 0 && section < this.sectionListRef.props.sections.length) {
           this.scrollToSection(section);
@@ -131,7 +60,7 @@ export default class App extends Component {
       <View style={styles.container} {...this._panResponder.panHandlers}>
         <StatusBar hidden />
         <View style={(styles.statBar, styles.skyblue)}>
-          <Text style={styles.statBar}>{this.state.zone}</Text>
+          <Text style={styles.statBar}>TEST</Text>
         </View>
         <View style={styles.listContainer}>
           <View style={{ flexDirection: 'column', flex: 1 }}>
